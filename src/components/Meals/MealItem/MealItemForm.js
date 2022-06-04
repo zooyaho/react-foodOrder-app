4320ff6e28b9e3aae6d79aeaa90cf7d0fe288c1e
zooyaho <jiwoo1268@naver.com>
2022-06-04 14:23:47 +0900

    MealItemForm.js에서 input 프롭 추가

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,13 +1,20 @@
 /* 사용자가 수량을 입력할 수 있는 폼(장바구니에 담고 싶은 음식의 수량)*/
 import React from 'react';
-import Input from '../../UI/Input';
 
 import classes from './MealItemtForm.module.css';
+import Input from '../../UI/input';
 
 const MealItemForm = props =>{
   return (
     <form className={classes.form}>
-      <Input />
+      <Input label="Amount" input={{
+        id:'amount',
+        type:'number',
+        min:'1',
+        max:'5',
+        step:'1',
+        defaultValue:'1'
+      }}/>
       <button>+ Add</button>
     </form>
   );
