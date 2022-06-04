4278b040ce2a43b0bc8c41e68efd7ad7ed5b4227
zooyaho <jiwoo1268@naver.com>
2022-06-04 12:07:08 +0900

    CartIcon SVG 컴포넌트 구현 후 추가,HeaderCartButton 컴포넌트에 추가

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,9 +1,10 @@
+import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css'
 
 const HeaderCartButton = (props) => {
   return (
     <button className={classes.button}>
-      <span></span>
+      <CartIcon />
       <span>Your Cart</span>
       <span className={classes.badge}>{/* 장바구니에 담긴 상품의 총량 */}3</span>
     </button>
