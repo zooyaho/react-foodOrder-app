97bc426b7e62de9e4b2fd81e4410256d910db9db
zooyaho <jiwoo1268@naver.com>
2022-06-04 14:52:58 +0900

    Cart컴포넌트에 더미데이터 추가

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,30 @@
-import React from 'react';
-import classes from './Cart.module.css';
+/* 장바구니 모달창 컴포넌트 */
+import React from "react";
+import classes from "./Cart.module.css";
 
-const Cart = ()=>{
-  return(
-    <div className={classes['cart-items']}>
+const Cart = (props) => {
+  // 장바구니 항목(더미데이터)
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {[{ id: "C1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
 
+  return (
+    <div>
+      {cartItems}
+      <div className={classes.total}>
+        {/* 총 수량 -> 하드코딩 해둠.*/}
+        <span>Total Amount</span>
+        <span>35.62</span>
+      </div>
+      {/* 장바구니에 대한 action: 닫기 및 주문 버튼 */}
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
     </div>
   );
 };
