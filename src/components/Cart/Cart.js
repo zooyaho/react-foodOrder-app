0fb70122ccd0c0043080ff80ade06337b7b1ac5d
zooyaho <jiwoo1268@naver.com>
2022-06-04 14:34:46 +0900

    Cart컴포넌트, 스타일 추가

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './Cart.module.css';
+
+const Cart = ()=>{
+  return(
+    <div className={classes['cart-items']}>
+
+    </div>
+  );
+};
+
+export default Cart;
