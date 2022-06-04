1f176b5b2666c01d990bc6b17e6ee975364aa33c
zooyaho <jiwoo1268@naver.com>
2022-06-04 14:15:58 +0900

    input 컴포넌트 추가, 스타일 구현 후 MealItemForm에 추가

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,12 +1,14 @@
 /* 사용자가 수량을 입력할 수 있는 폼(장바구니에 담고 싶은 음식의 수량)*/
 import React from 'react';
+import Input from '../../UI/Input';
+
 import classes from './MealItemtForm.module.css';
 
 const MealItemForm = props =>{
   return (
     <form className={classes.form}>
-      <input />
-      <button></button>
+      <Input />
+      <button>+ Add</button>
     </form>
   );
 };
---
+++ src/components/UI/input.js
@@ -0,0 +1,14 @@
+import React from "react";
+import classes from "./Input.module.css";
+
+const input = (props) => {
+  return (
+    <div className={classes.input}>
+      <label htmlFor={props.input.id}>{props.label}</label>
+      <input {...props.input}></input>
+      {/* 요소에 전개연산자 사용 -> {...props.input}: props.input에서 받아온 이 input객체에 있는 모든 키-값 쌍을 프롭으로 input에 추가할 수 있음!!, id={props.input.id} 생략 가능 */}
+    </div>
+  );
+};
+
+export default input;
