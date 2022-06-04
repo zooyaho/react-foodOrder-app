3d4d56d7f5808d6a50ca2f37c497a6e3144a102a
zooyaho <jiwoo1268@naver.com>
2022-06-04 14:31:59 +0900

    input id값 수정

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -8,7 +8,7 @@ const MealItemForm = props =>{
   return (
     <form className={classes.form}>
       <Input label="Amount" input={{
-        id:'amount',
+        id:'amount_' + props.id,
         type:'number',
         min:'1',
         max:'5',
