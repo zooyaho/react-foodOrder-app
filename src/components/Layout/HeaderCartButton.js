d7ac81461a614adf92635786e9a6b88b5994ae6a
zooyaho <jiwoo1268@naver.com>
2022-06-05 16:16:01 +0900

    HeaderCartButton에서 cart context의 amount값 가져와 배지에 업데이트

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,12 +1,24 @@
-import CartIcon from '../Cart/CartIcon';
-import classes from './HeaderCartButton.module.css'
+import React, { useContext } from "react";
+
+import CartIcon from "../Cart/CartIcon";
+import CartCoctext from "../../store/cart-context";
+import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartCoctext);
+
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.ammount;
+  }, 0);
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <CartIcon />
       <span>Your Cart</span>
-      <span className={classes.badge}>{/* 장바구니에 담긴 상품의 총량 */}3</span>
+      <span className={classes.badge}>
+        {/* 장바구니에 담긴 상품의 총량 */}
+        {numberOfCartItems}
+      </span>
     </button>
   );
 };
