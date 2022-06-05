8aad44a26e4419cd109fe2e5af21ef36bb8f8e3a
zooyaho <jiwoo1268@naver.com>
2022-06-05 18:37:59 +0900

    장바구니 모달창에 업데이트되는 totalAmount값으로 교체, 장바구니에 음식이 추가되어야 order버튼 보여지게 구현 완료

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,30 +1,33 @@
 /* 장바구니 모달창 컴포넌트 */
-import React from "react";
+import React, {useContext} from "react";
+import CartContext from "../../store/cart-context";
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
 
 const Cart = (props) => {
-  // 장바구니 항목(더미데이터)
+  const cartCtx = useContext(CartContext);
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
-      {[{ id: "C1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
+      {cartCtx.items.map((item) => (
         <li>{item.name}</li>
       ))}
     </ul>
   );
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasOrder = cartCtx.items.length > 0;
 
   return (
     <Modal onClose={props.onClose}>
       {cartItems}
       <div className={classes.total}>
-        {/* 총 수량 -> 하드코딩 해둠.*/}
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{totalAmount}</span>
       </div>
       {/* 장바구니에 대한 action: 닫기 및 주문 버튼 */}
       <div className={classes.actions}>
         <button className={classes["button--alt"]} onClick={props.onClose}>Close</button>
-        <button className={classes.button}>Order</button>
+        {hasOrder && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
