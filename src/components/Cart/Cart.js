c64be25790ec1ff2884b0e6b3e716ec322e98e09
zooyaho <jiwoo1268@naver.com>
2022-06-05 18:48:37 +0900

    CartItem 컴포넌트, 스타일 추가 후 Cart컴포넌트에 연결

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,16 +1,27 @@
 /* 장바구니 모달창 컴포넌트 */
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import CartContext from "../../store/cart-context";
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
+import CartItem from "./CartItem";
 
 const Cart = (props) => {
   const cartCtx = useContext(CartContext);
 
+  // 장바구니에서 항목을 추가하거나 삭제하는 함수
+  const cartItemRemoveHandler = (id) => {};
+  const cartItemAddHandler = (item) => {};
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item) => (
-        <li>{item.name}</li>
+        <CartItem
+          name={item.name}
+          price={item.price}
+          amount={item.amount}
+          onRemove={cartItemRemoveHandler}
+          onAdd={cartItemAddHandler}
+        />
       ))}
     </ul>
   );
@@ -26,7 +37,9 @@ const Cart = (props) => {
       </div>
       {/* 장바구니에 대한 action: 닫기 및 주문 버튼 */}
       <div className={classes.actions}>
-        <button className={classes["button--alt"]} onClick={props.onClose}>Close</button>
+        <button className={classes["button--alt"]} onClick={props.onClose}>
+          Close
+        </button>
         {hasOrder && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
